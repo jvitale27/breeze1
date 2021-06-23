34c2fd798efe9fa761230ccb28482aa94ea5a8ee
Julian Vitale <jvitale27@gmail.com>
2021-06-23 19:53:18 -0300

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- 'mi_css' styles -->
+        {{--slot para definir estilos desde plantillas. Es para que funcione Dropzone, etc. --}}
+        @if (isset($mi_css))
+            {{ $mi_css }}
+        @endif
+
+        <!-- stack 'css' styles -->
+        {{-- aqui puedo incluir codigo 'css' con la sintaxis push('css') de blade, desde mi {{ $slot }} principal --}}
+        @stack('css')
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        <!-- 'mi_js' scripts -->
+        {{-- slot para ejecutar scripts desde cualquier plantilla --}}
+        @if (isset($mi_js))
+            {{ $mi_js }}        {{-- slot para ejecutar scripts desde cualquier plantilla --}}
+        @endif
+
+        <!-- stack 'js' scripts -->
+        {{-- aqui puedo incluir codigo 'js' con la sintaxis push('js') de blade, desde mi {{ $slot }} principal --}}
+        @stack('js')
+        
+    </body>
+</html>
